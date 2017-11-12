41200e76de6d16d263cd28ba3a565615bceb972d
Ed <edoisa6@gmail.com>
2017-11-11 17:13:13 -0800

    first commit

---
+++ EmptyQueueException.java
@@ -0,0 +1,15 @@
+/**
+ * Thrown by methods in the <code>Queue</code> class to indicate
+ * that the queue is empty.
+ */
+
+public class EmptyQueueException extends RuntimeException {
+	
+	/**
+	 * Constructs a new <code>EmptyQueueException</code> with <tt>null</tt>
+	 * as its error message string.
+	 */
+	public EmptyQueueException() {
+	}
+	
+}
---
+++ SingleLinkedPriorityQueue.java
@@ -0,0 +1,82 @@
+
+
+/**
+ * A class implementing a Priority Queue using single linked chain
+ */
+
+public final class SingleLinkedPriorityQueue<T> implements PriorityQueueInterface<T> {
+	// member variables
+	private Node frontNode;
+	private Node backNode;
+
+	public SingleLinkedPriorityQueue() {
+		frontNode = null;
+		backNode = null;
+	}
+
+	/** Adds a new entry to this priority queue.
+	 @param newEntry  An object to be added. */
+	public void add(T newEntry) {
+
+	}
+
+	/** Removes and returns the entry having the highest priority.
+	 @return  Either the object having the highest priority or,
+	 if the priority queue is empty before the operation, null. */
+	public T remove() {
+
+	}
+
+	/** Retrieves the entry having the highest priority.
+	 @return  Either the object having the highest priority or,
+	 if the priority queue is empty, null. */
+	public T peek() {
+
+	}
+
+	/** Detects whether this priority queue is empty.
+	 @return  True if the priority queue is empty, or false otherwise. */
+	public boolean isEmpty() {
+
+	}
+
+	/** Gets the size of this priority queue.
+	 @return  The number of entries currently in the priority queue. */
+	public int getSize() {
+
+	}
+
+	/** Removes all entries from this priority queue. */
+	public void clear() {
+
+	}
+
+	/** Implements Node class */
+	private class Node {
+		private T data;
+		private Node next;
+
+		Node(T data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
+
+		public T getData() {
+			return data;
+		}
+
+		private void setData(T newData) {
+			data = newData;
+		}
+
+		public Node getNextNode() {
+			return next;
+		}
+
+		private void setNextNode(Node newNode) {
+			next = newNode;
+		}
+
+	}
+
+}
